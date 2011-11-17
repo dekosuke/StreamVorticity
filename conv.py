f5b5018333ca3a19189d3a346b87b1a9f300ca25
JUN komiyama <deco.elb@gmail.com>
2011-11-17 19:22:43 +0900

    changed draw method / added highres image

--- conv.py
+++ conv.py
@@ -19,22 +19,36 @@ def writeFile(mesh, num):
   img = Image.new('RGB', (X,Y))
   sumc=0
 
+  cs=[]
   print "avg=",avg(mesh, X, Y)
-  for j in range(Y):
+  for i in range(X):
     prev_v = 0
-    for i in range(X):
+    cs.append([])
+    for j in range(Y):
+      cs[-1].append(0)
       #c = min(max(abs(int(mesh[i][j]*50)), 0), 255)
       #c = min(abs(int(mesh[i][j]*100)), 255)
       v = int(mesh[i][j]*100)
       if v>prev_v:
-        c=128
+        cs[i][j]=128
         prev_v = v
       elif v<prev_v:
-        c=255
+        cs[i][j]=255
         prev_v = v
       else:
-        c=0
+        pass#c=0
+  for j in range(Y):
+    prev_v=0;
+    for i in range(X):
+      v = int(mesh[i][j]*100)
+      if v>prev_v:
+        cs[i][j]=128
+        prev_v = v
+      elif v<prev_v:
+        cs[i][j]=255
+        prev_v = v
       sumc+=v/255.0
+      c=cs[i][j]
       img.putpixel( (i,j), (255,c,255-c) )
   img.save("data/img"+str(num)+".png", "PNG")
   return sumc/(X*Y)
